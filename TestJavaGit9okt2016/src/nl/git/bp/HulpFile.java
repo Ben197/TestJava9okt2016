aadb57606a738d4cd33689ee9ee749a6a4b38e5f
Ben197 <jabro888uc@Hotmail.com>
2016-10-09 14:12:50 +0200

    Initiele checkin

---
+++ TestJavaGit9okt2016/src/nl/git/bp/HulpFile.java
@@ -0,0 +1,15 @@
+package nl.git.bp;
+
+public class HulpFile {
+	
+	public int testDezeFile(int getal1, int getal2)
+	{
+		System.out.println("Getal1: " + getal1);
+		System.out.println("Getal2: " + getal2);
+		int som = getal1 + getal2;
+		return som;
+		
+				
+	}
+
+}
---
+++ TestJavaGit9okt2016/src/nl/git/bp/StartOp.java
@@ -0,0 +1,12 @@
+package nl.git.bp;
+
+public class StartOp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HulpFile hf = new HulpFile();
+		System.out.println("wijziging 1");
+		System.out.println("Uitkomst: " + hf.testDezeFile(1, 3));
+	}
+
+}
