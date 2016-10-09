88987246b9edd4139e59187dc040c92856d091d3
Ben197 <jabro888uc@Hotmail.com>
2016-10-09 15:59:45 +0200

    Wijziging 5

--- TestJavaGit9okt2016/src/nl/git/bp/StartOp.java
+++ TestJavaGit9okt2016/src/nl/git/bp/StartOp.java
@@ -8,7 +8,7 @@ public class StartOp {
 		System.out.println("wijziging 1");
 		System.out.println("Uitkomst: " + hf.testDezeFile(1, 3));
 		System.out.println("wijziging 2");
-		System.out.println("wijziging 3");
+		System.out.println("wijziging 5");
 	}
 
 }
